4f062f9608290b620c4cb7895e30bc7da3d81f76
Chris Koepke <ckoepke@innovetech.com>
2009-08-02 18:12:23 -0400

    trying to prevent browser caching

--- rssbotd.sh
+++ rssbotd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 Time=540
-while true
-do
-	if ping -c3 www.google.com
+while true; do
+	if ping -c3 www.google.com 2>&1 >/dev/null
 		then
-		/var/www/cgi-bin/rssbot.py
+		/var/www/cgi-bin/rssbot.py 2>&1 >/dev/null
 		wait
+	fi
 	sleep $Time
 done
 exit 1
