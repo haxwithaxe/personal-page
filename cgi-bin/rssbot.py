64fd46fea95c2d70b1ff20dc4bc84685bb2fedb2
Chris Koepke <mannkoepke@gmail.com>
2009-10-07 00:42:49 -0400

    now finding links in the title and making them real

--- cgi-bin/rssbot.py
+++ cgi-bin/rssbot.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import feedparser, string, cgitb
+import feedparser, string, re, cgitb
 cgitb.enable()
-
-FEEDS = ['http://mannkoepke:mytvv1tt3r@twitter.com/statuses/friends_timeline/15577540.rss']
-
-def rss(feeds):
-   r = ''
-   for i in feeds:
-      f = feedparser.parse(i)
-      e = f.entries
-      r += ('<span id=ftitle>'+f.feed.title +'</b><br/>')
-      for o in e:
-	 r += ('<span id=etitle>'+o.title+'</span><a href="'+o.link+'">link</a><br/>')
-   return r
 
 print("Content-Type: text/html\n")
 
-print(rss(FEEDS).decode('utf-8', 'ignore'))
+
+FEEDS = ['http://mannkoepke:mytvv1tt3r@twitter.com/statuses/friends_timeline/15577540.rss']
+urlre = re.compile('http://.*\.[a-z,A-Z]*/[a-z,A-Z,0-9]*[^\s ]')
+
+
+def rss(feeds):
+   for i in feeds:
+      f = feedparser.parse(i)
+      e = f.entries
+      print('<span id="ftitle"><b>'+f.feed.title +'</b></span><br/>')
+      for o in e:
+	 print('<div id="etitle">')
+	 etitle = o.title
+	 urls = re.findall(urlre,etitle)
+	 for u in urls:
+	    etitle = re.sub(u,'<a id="inline" target="_blank" href="'+u+'">'+u+'</a>', etitle)
+	 print etitle
+	 print(' <a href="'+o.link+'">link</a></div><br/>')
+
+rss(FEEDS)
