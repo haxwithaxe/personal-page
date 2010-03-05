f7f3d1d19b3336b02c690ee2c7277d95ab23d43c
hax <hax@hddbuntu.(none)>
2010-03-05 10:12:26 -0500

    updating: it's been a while

--- cgi-bin/rssbot.py
+++ cgi-bin/rssbot.py
@@ -4,24 +4,39 @@ import feedparser, string, re, cgitb
 cgitb.enable()
 
 print("Content-Type: text/html\n")
+########## TODO ########################################################
+# add mailto links regex=(\w+@[a-zA-Z]{2,6})
+#
+########################################################################
 
-
-FEEDS = ['http://mannkoepke:passwd@twitter.com/statuses/friends_timeline/15577540.rss']
-urlre = re.compile('http://.*\.[a-z,A-Z]*.[a-z,A-Z,0-9]*[^\s ]')
-
+FEEDS = ['https://mannkoepke:^^ytvv1tt3r@twitter.com/statuses/friends_timeline/15577540.rss']
+urlre = re.compile('http[s]?://[\S]+')
+mailre = re.compile("[a-z,A-Z,.]+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}")
 
 def rss(feeds):
    for i in feeds:
       f = feedparser.parse(i)
       e = f.entries
-      print('<span id="ftitle"><b>'+f.feed.title +'</b></span><br/>')
+      try:
+         print('<span id="ftitle"><b>'+f.feed.title +'</b></span><br/>')
+      except:
+         print('<span id="ftitle"><b>RSS Feeds</b></span><br/>')
       for o in e:
 	 print('<div id="etitle">')
-	 etitle = o.title+' '
+	 try:
+            etitle = o.title+' '
+         except:
+            etitle = 'No Message'
 	 urls = re.findall(urlre,etitle)
 	 for u in urls:
 	    try:
-	       etitle = re.sub(u,'<a id="inline" target="_blank" href="'+u+'">'+u+'</a>', etitle)
+	       etitle = re.sub(re.escape(u),'<a id="inline" target="_blank" href="'+u+'">'+u+'</a>', etitle)
+	    except:
+	       continue
+         mailtos = re.findall(mailre,etitle)
+         for m in mailtos:
+	    try:
+	       etitle = re.sub(re.escape(m),'<a id="mailto" href="mailto:'+m+'">'+m+'</a>', etitle)
 	    except:
 	       continue
 	 print(etitle.encode("utf-8"))
