41e61f653489d1a9aec6cdde3e94b549eccaa73e
Chris Koepke <ckoepke@innovetech.com>
2011-01-15 03:20:27 -0500

    fixed index.py up and moved some stuff around

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python
 # index script will determine permissions to view subsequent pages
 # and redirect to the appropriate page.
-import cgi ,cgitb
+import cgi
 
-def main():
-	ReDir = /cgi-bin/welcome.py
-	
-	retrun ReDir
+TMPL_DIR = '../tmpl/'
 
-echo main()
+CONTENT_DIR = '../content/'
+
+tmpl = open(TMPL_DIR+'page','r').read()
+
+lsidebar = open(TMPL_DIR+'lsidebar','r').read()
+
+rsidebar = open(TMPL_DIR+'rsidebar','r').read()
+
+form = cgi.FieldStorage()
+
+if form.has_key('q'):
+
+	page = form['q'].value
+
+else:
+
+	page = 'home'
+
+content = open(CONTENT_DIR+page,'r').read()
+
+print('Content-Type: text/html\n\n\n')
+
+print(tmpl.replace('<!--CONTENT-->',content).replace('<!--LSIDEBAR-->',lsidebar).replace('<!--RSIDEBAR-->',rsidebar))
